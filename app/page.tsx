610684ce00a6ff320744ecd7b8d3351a6d025d6e
Anton.Happel <info@antonhappel.com>
2024-01-24 17:20:50 +0100

    INIT

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from "next"
 import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
+import {Product} from "components/Product/Product";
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: "DealDaddy.de",
   twitter: {
     card: "summary_large_image",
   },
@@ -20,14 +21,31 @@ export const metadata: Metadata = {
 }
 
 export default function Web() {
+
+  const products = [
+    {
+      image: 'https://static.mydealz.de/threads/raw/Q2nsR/2308043_1/re/300x300/qt/60/2308043_1.jpg',
+      title: 'FRANZIS 67157 - Der große Technikbausatz Hybridmotor - (inkl. Motor Soundmodul / Motorbausatz im Maßstab 1:3 / 110 Teile / Begleitbuch))'
+    },
+    {
+      image: 'https://static.mydealz.de/threads/raw/WF5RM/2308044_1/re/300x300/qt/60/2308044_1.jpg'
+    },
+  ]
+
+
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
+        <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              DealDaddy.de
             </h1>
+            <section className="grid gap-4">
+              { products.map((product, index) => (
+                  (<Product key={index} image={product.image} title={product.title} />)
+              ))}
+            </section>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
               Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
@@ -37,26 +55,11 @@ export default function Web() {
               Get started
             </Button>
             <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
-              intent="secondary"
+                href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+                intent="secondary"
             >
               Deploy Now
             </Button>
-          </div>
-        </div>
-      </section>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
-          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
-            {LP_GRID_ITEMS.map((singleItem) => (
-              <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
-                  {singleItem.icon}
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
-              </div>
-            ))}
           </div>
         </div>
       </section>
---
+++ components/Product/Product.tsx
@@ -0,0 +1,82 @@
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { twMerge } from "tailwind-merge"
+
+const button = cva(
+  [
+    "justify-center",
+    "inline-flex",
+    "items-center",
+    "rounded-xl",
+    "text-center",
+    "border",
+    "border-blue-400",
+    "transition-colors",
+    "delay-50",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: ["bg-blue-400", "text-white", "hover:enabled:bg-blue-700"],
+        secondary: ["bg-transparent", "text-blue-400", "hover:enabled:bg-blue-400", "hover:enabled:text-white"],
+      },
+      size: {
+        sm: ["min-w-20", "h-full", "min-h-10", "text-sm", "py-1.5", "px-4"],
+        lg: ["min-w-32", "h-full", "min-h-12", "text-lg", "py-2.5", "px-6"],
+      },
+      underline: { true: ["underline"], false: [] },
+    },
+    defaultVariants: {
+      intent: "primary",
+      size: "lg",
+    },
+  }
+)
+
+export interface ProductProps extends React.ButtonHTMLAttributes<HTMLAnchorElement>, VariantProps<typeof button> {
+  underline?: boolean
+  image: string
+  title: string
+}
+
+export function Product({ className, intent, size, underline, ...props }: ProductProps) {
+  return (
+      <article
+          className="relative flex flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs min-w-full mx-auto border border-white bg-white">
+          <div className="w-1/3 bg-white grid place-items-center">
+              <img
+                  src={props.image}
+                  alt="tailwind logo" className="rounded-xl"/>
+          </div>
+          <div className="w-2/3 bg-white flex flex-col space-y-2 p-3">
+              <div className="flex justify-between item-center">
+                  <p className="text-gray-500 font-medium hidden md:block">Vacations</p>
+                  <div className="flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+                           fill="currentColor">
+                          <path
+                              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                      </svg>
+                      <p className="text-gray-600 font-bold text-sm ml-1">
+                          4.96
+                          <span className="text-gray-500 font-normal">(76 reviews)</span>
+                      </p>
+                  </div>
+                  <div
+                      className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
+                      Superhost
+                  </div>
+              </div>
+              <h3 className="font-black text-gray-800 text-l text-left truncate">{props.title}</h3>
+              <p className="text-gray-500 text-base">The best kept secret of The Bahamas is the country’s
+                  sheer
+                  size and diversity. With 16 major islands, The Bahamas is an unmatched destination</p>
+              <p className="text-xl font-black text-gray-800">
+                  $110
+                  <span className="font-normal text-gray-600 text-base">/night</span>
+              </p>
+          </div>
+      </article>
+
+  )
+}
